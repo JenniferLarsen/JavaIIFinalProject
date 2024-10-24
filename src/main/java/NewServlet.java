9b4bae34ae0a5dba44ea6b18a9a4e93df24d0fce
Casey Kluesner <casey.j.kluesner@gmail.com>
2024-10-23 19:19:52 -0500

    Product Page

---
+++ src/main/java/NewServlet.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author casey
+ */
+@WebServlet(urlPatterns = {"/NewServlet"})
+public class NewServlet extends HttpServlet {
+
+   
+       @Override
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // get parameters from the request
+        String productString = request.getParameter("product");
+        String descriptionString = request.getParameter("description");
+        String priceString = request.getParameter("price");
+
+        // validate the parameters
+        String url;
+        String message;
+        String product ;
+        String description;
+        double price = 0;            
+        try {
+            product = productString;
+            description = descriptionString;
+            price = Double.parseDouble(priceString);            
+            message = "";
+            url = "/product.jsp";
+        }
+        catch (NumberFormatException e) {
+            message = "Please enter a valid price.";
+            url = "/product.jsp";
+        }
+
+      
+            
+        getServletContext()
+                .getRequestDispatcher(url)
+                .forward(request, response);
+    }
+    
+  
+
+}
